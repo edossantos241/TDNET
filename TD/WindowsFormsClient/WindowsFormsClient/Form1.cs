e5b89b39b79624e0378740a2245bd4616da5c1b9
Emma Dos Santos <emmados@orange.fr>
2020-05-20 11:33:39 +0200

    TD1

---
+++ TD/WindowsFormsClient/WindowsFormsClient/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsClient
+{
+    public partial class Form1 : Form
+    {
+        private RemotingObjects.IRemotingObjects remoteObject;
+        public Form1()
+        {
+            try
+            {
+                TcpChannel channel = new TcpChannel();
+                ChannelServices.RegisterChannel(channel, true);
+                remoteObject = (RemotingObjects.IRemotingObjects)
+                    Activator.GetObject(typeof(RemotingObjects.IRemotingObjects),
+                    "tcp://localhost:12000/HostObject");
+            }
+            catch
+            {
+                MessageBox.Show("Error connecting to the server !");
+            }
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (remoteObject != null)
+            {
+                String result = remoteObject.sayHello(textBox1.Text);
+                MessageBox.Show("Result: " + result);
+            } else
+            {
+                MessageBox.Show("Error: remote object not available");
+            }
+        }
+    }
+}
